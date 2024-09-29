a66cd90f195ec776d41e67a5608ab244b3c28920
archishhaa <archishasharma3@gmail.com>
2024-09-29 15:56:27 +0530

    created

---
+++ project1/script.js
@@ -0,0 +1,8 @@
+document.getElementById("change-city").addEventListener("click", function() {
+   
+    alert("City change functionality not implemented yet.");
+});
+
+
+document.getElementById('current-temp').innerText = '1°C'; // Replace this with dynamic data
+document.getElementById('city').innerText = 'Beijing'; // Replace this with dynamic data
